f14ecfc839339b3018dc10494363848ab30d921c
kshukla85 <45736379+kshukla85@users.noreply.github.com>
2026-01-02 23:29:51 +0000

    adding files

---
+++ foo/src/bin/main_2.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut message = String::from("Name: Alfredo, Height: ");
+    message.clear();
+    let mut height = 190.0;
+    height = 189.0;
+    println!("{}{}", message, height);
+}
---
+++ foo/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let message = "Alfredo, weight";
+    let weight = 190.0;
+
+    let kilos = weight / 2.2;
+    println!("{}{}", message, weight);
+    println!("{}{}", message, kilos);
+}
+
+
